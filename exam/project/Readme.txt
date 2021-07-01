181c1dfe97589983f1a2b45a945e93c30f48bf63
Angel Angelov <64867029+AngelValAngelov@users.noreply.github.com>
2021-07-02 00:47:46 +0300

    Add files via upload

---
+++ exam/project/Readme.txt
@@ -0,0 +1,5 @@
+Players and Monsters is a battle game. 
+It's all about battles between players with their cards. 
+Each player has health and deck of cards. 
+Each card gives bonus damage and bonus health. 
+The players fight on the battlefield with their cards. 
---
+++ exam/project/player/player_repository.py
@@ -0,0 +1,31 @@
+from project.player.player import Player
+
+
+class PlayerRepository:
+    def __init__(self):
+        self.players = []
+
+    def add(self, player: Player):
+        if any(p.username == player.username for p in self.players):
+            raise ValueError(f"Player {player.username} already exists!")
+        '''
+        player_name = [p for p in self.players if p.username == player.username]
+        if player_name:
+            raise ValueError(f"Player {player.username} already exists!")
+            '''
+        self.players.append(player)
+
+    @property
+    def count(self):
+        return len(self.players)
+
+    def remove(self, player_name: str):
+        if player_name == "":
+            raise ValueError("Player cannot be an empty string!")
+        found_player = [player for player in self.players if player.username == player_name][0]
+        self.players.remove(found_player)
+
+    def find(self, username: str):
+        found_player = [player for player in self.players if player.username == username][0]
+        return found_player
+
